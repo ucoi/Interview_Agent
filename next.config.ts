f05c29682c463354754434095dcf6f794b653422
Ahmed Hisham <ahmedhisham1928@gmail.com>
2026-09-08 21:47:41 +0300

    fixed vercel server crash 4 i hope it works

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["firebase-admin"],
+  serverExternalPackages: ["firebase-admin", "jwks-rsa", "jose"],
   allowedDevOrigins: ["192.168.8.*"],
 }
 
